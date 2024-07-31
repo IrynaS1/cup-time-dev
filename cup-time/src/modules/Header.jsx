5a7b8c314c73a37744caea3b2553284c2ae40abb
IrynaS1 <snowqueen-2007@mail.ru>
2024-07-31 23:12:14 +0300

    vite and react on, add responsive styles

---
+++ cup-time/src/modules/Header.jsx
@@ -0,0 +1,40 @@
+export const Header = () => {
+	return (
+		<header className="header">
+			<div className="container header__container">
+				<a className="header__logo-link">
+					<img src="./image/logo.svg" alt="Логотип Cup Time" className="header__logo" />
+				</a>
+
+				<nav className="header__nav">
+					<ul className="header__menu">
+						<li className="header__menu-item">
+							<a href="#" className="header__menu-link">Чай</a>
+						</li>
+						<li className="header__menu-item">
+							<a href="#" className="header__menu-link">Кофе</a>
+						</li>
+						<li className="header__menu-item">
+							<a href="#" className="header__menu-link">Чайники</a>
+						</li>
+						<li className="header__menu-item">
+							<a href="#" className="header__menu-link">Турки</a>
+						</li>
+						<li className="header__menu-item">
+							<a href="#" className="header__menu-link">Прочее</a>
+						</li>
+					</ul>
+				</nav>
+
+				<div className="header__links">
+					<a href="cart.html" className="header__cart-link">6</a>
+
+					<button className="header__menu-btn"><svg width="20" height="12" viewBox="0 0 20 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<rect y="0.5" width="20" height="1" fill="#D9D9D9" />
+						<rect y="5.5" width="20" height="1" fill="#D9D9D9" />
+						<rect y="10.5" width="20" height="1" fill="#D9D9D9" />
+					</svg></button></div>
+			</div>
+		</header>
+	);
+};
